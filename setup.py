5b6dccbb9a1f97df4e1baba26a09b51e7039599c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-30 01:23:03 +0000

    pep8;remove unused module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.wref/trunk@105395 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,54 +4,54 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	'console_scripts': [
-	],
+    'console_scripts': [
+    ],
 }
 
 TESTS_REQUIRE = [
-	'nose',
-	'nose-timer',
-	'nose-pudb',
-	'nose-progressive',
-	'nose2[coverage_plugin]',
-	'pyhamcrest',
+    'nose',
+    'nose-timer',
+    'nose-pudb',
+    'nose-progressive',
+    'nose2[coverage_plugin]',
+    'pyhamcrest',
 ]
 
 setup(
-	name='nti.wref',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI Weak References",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='Weak References',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Programming Language :: Python :: Implementation :: PyPy'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	tests_require=TESTS_REQUIRE,
-	install_requires=[
-		'setuptools',
-		'persistent',
-		'zope.component',
-		'zope.interface',
-		'zope.security'
-	],
-	extras_require={
-		'test': TESTS_REQUIRE,
-	},
-	entry_points=entry_points
+    name='nti.wref',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Weak References",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='Weak References',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'persistent',
+        'zope.component',
+        'zope.interface',
+        'zope.security'
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points
 )
