b7178078059d3035ff555b3ace6b199436628993
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-30 00:56:27 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.wref/trunk@105394 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/wref/interfaces.py
+++ src/nti/wref/interfaces.py
@@ -13,36 +13,37 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
+
 class IWeakRef(interface.Interface):
-	"""
-	Represents a weak reference to some object. The strategy for
-	creating and maintaining weak references may vary dramatically for
-	different types of objects, so the semantics associated with cleanup
-	should not be assumed. For example, weak references to ordinary python
-	objects can exist only within the lifetime of a single process
-	and clear immediately when the original object is gone, but weak
-	references to persistent objects may last for several processes and
-	may persist even after the original object is gone.
-	"""
+    """
+    Represents a weak reference to some object. The strategy for
+    creating and maintaining weak references may vary dramatically for
+    different types of objects, so the semantics associated with cleanup
+    should not be assumed. For example, weak references to ordinary python
+    objects can exist only within the lifetime of a single process
+    and clear immediately when the original object is gone, but weak
+    references to persistent objects may last for several processes and
+    may persist even after the original object is gone.
+    """
 
-	def __call__():
-		"""
-		Weak references are callable objects. Calling them returns
-		the object they reference if it is still available, otherwise
-		it returns ``None``.
-		"""
+    def __call__():
+        """
+        Weak references are callable objects. Calling them returns
+        the object they reference if it is still available, otherwise
+        it returns ``None``.
+        """
 
-	def __eq__(other):
-		"""
-		Weak references should be equal to other objects that
-		weakly reference the same object.
-		"""
+    def __eq__(other):
+        """
+        Weak references should be equal to other objects that
+        weakly reference the same object.
+        """
 
-	def __hash__():
-		"""
-		Weak references should be suitable for hashing.
-		If possible, they should hash the same as the underlying object.
-		"""
+    def __hash__():
+        """
+        Weak references should be suitable for hashing.
+        If possible, they should hash the same as the underlying object.
+        """
 
 import weakref
 interface.classImplements(weakref.ref, IWeakRef)
@@ -50,34 +51,36 @@ interface.classImplements(weakref.ref, IWeakRef)
 import persistent.wref
 interface.classImplements(persistent.wref.WeakRef, IWeakRef)
 
+
 class IWeakRefToMissing(IWeakRef):
-	"""
-	A weak reference that knows enough about the object it was
-	referencing to be able to produce a :const:`.TYPE_MISSING` NTIID
-	when the reference is clear (when calling this object returns ``None``).
-	"""
+    """
+    A weak reference that knows enough about the object it was
+    referencing to be able to produce a :const:`.TYPE_MISSING` NTIID
+    when the reference is clear (when calling this object returns ``None``).
+    """
 
-	def make_missing_ntiid():
-		"""
-		Call this when the reference is clear to produce an NTIID
-		that refers to the object that was referenced.
+    def make_missing_ntiid():
+        """
+        Call this when the reference is clear to produce an NTIID
+        that refers to the object that was referenced.
 
-		Calling this before the reference is clear is not defined.
-		"""
+        Calling this before the reference is clear is not defined.
+        """
+
 
 class ICachingWeakRef(IWeakRef):
-	"""
-	A weak ref that, as an implementation detail, may cache the referant.
-	Whether or not to use that cached value is exposed as a keyword argument.
-	"""
+    """
+    A weak ref that, as an implementation detail, may cache the referant.
+    Whether or not to use that cached value is exposed as a keyword argument.
+    """
 
-	def __call__( allow_cached=True ):
-		"""
-		Resolve the reference, as with :meth:`IWeakRef.call`; however, allows
-		control of caching.
+    def __call__(allow_cached=True):
+        """
+        Resolve the reference, as with :meth:`IWeakRef.call`; however, allows
+        control of caching.
 
-		:keyword allow_cached: If `True` (the default) a cached value can be used. If
-			set to `False`, this will not return a cached value. Note, however, that,
-			in a cluster, this may still return an object that other nodes might consider
-			gone.
-		"""
+        :keyword allow_cached: If `True` (the default) a cached value can be used. If
+                set to `False`, this will not return a cached value. Note, however, that,
+                in a cluster, this may still return an object that other nodes might consider
+                gone.
+        """
